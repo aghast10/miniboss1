935cf74f072535ad2bc1475c130d884d50195d2e
Nicolás Mallén <github.drdfo@slmails.com>
2025-07-28 07:57:16 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is my first program ever. learning to code.
 This program allows you to register your tasks and saves them. in order to use it, you must have python installed.
-for it to run, execute main.py within you terminal
+For it to run, execute main.py within your terminal.
--- tareas.py
+++ tareas.py
@@ -2,67 +2,78 @@ from archivo import saving_tasks, tasks
 from datetime import datetime
 
 def printed_menu():
-    print ("---MENU---")
-    print ("1. Ver tareas")
-    print ("2. Añadir tarea")
-    print ("3. Eliminar tarea")
-    print ("4. Mostrar Menu")
-    print ("5. Salir")
-    
+    menu = ["---MENU---",
+            "1. Ver tareas",
+            "2. Añadir tareas",
+            "3. Eliminar tarea",
+            "4. Salir"
+            ]
+    for opcion in menu: print(opcion)
+
 def choices():
-    choice = "0" #defino la variable choice para activar el bucle a continuación
-    while choice != "5":
-       
-        choice = input("Elige una opción: ") #modifica choice y sigue el script       
-        if choice == "1":
-            see_task() #activa la funcion see_task(), una vez leida, se reinicia el bucle
-        elif choice == "2":
-            add_task()      
-        elif choice == "3":
-            delete_task()
-        elif choice == "4":
+    opciones = {
+        "1": see_task,
+        "2": add_task,
+        "3": delete_task
+    }
+
+    choice = ""
+    while choice != "4":
+        choice = input("Elige una opción: ")
+        accion = opciones.get(choice) #opciones.get(choice) selecciona el elemento del diccionario si este existe
+        if choice == "4":
+            print("Has salido.")
+        elif accion: #si opciones(choice) existe:
+            accion() #si por ejemplo accion equivale a opciones("1"), entonces accion = see_task, y accion() = see_task()
             printed_menu()
-        elif choice == "5":
-            print ("Has salido") #como choice es "5", nada más imprimir esto se reinicia el bucle, pero como choice == 3, se sale del bucle y el programa termina (no hay nada más que hacer).
         else:
-            print ("Opción no válida. Inténtalo de nuevo.")
+            print("Opción no válida. Inténtalo de nuevo.")
  
 def see_task():
     if len(tasks) == 0:
         print("-------")
-        print("No hay ninguna tarea guardada. selecciona otra opción")
-        
-    else:
-        for i, task in enumerate(tasks):#enumerará todas las partes de la lista tasks
-            print (f"{i+1}. {task["title"]} -- {task["description"]} -- A completar antes del {task["deadline"]} --({task["status"]})") #por cada par índice-valor, se imprime una cadena, donde aparecerá el índice y el valor que se esté recorriendo en esa iteración.
-        print("Selecciona el numero de la tarea a completar. Si no, pulsa enter: ")
-        completed_task = input()
-        while completed_task != "":
-            try: 
-                tasks[int(completed_task)-1]["status"] = "Completada"
-                saving_tasks()
-                print("Tarea completada")
-                break
-            except (ValueError,IndexError): 
-                print("Selecciona una opción válida")
-                completed_task = input()
-        print("-------")
-        print("Filtrar por Completadas:'c', Filtrar por No completadas: 'nc', Ordenar por fecha límite: 'o', Salir a menu: Enter")
-        filter_tasks = input()
-        if filter_tasks == "c":
-            for i, task in enumerate(tasks):
-                if task["status"] == "Completada":
-                    print (f"{i+1}. {task["title"]} -- {task["description"]} -- A completar antes del {task["deadline"]} --({task["status"]})")
-        if filter_tasks == "nc":
-            for i, task in enumerate(tasks):
-                if task["status"] == "No Completada":
-                    print (f"{i+1}. {task["title"]} -- {task["description"]} -- A completar antes del {task["deadline"]} --({task["status"]})")
-        if filter_tasks == "o":
-            ordered_tasks = sorted(tasks, key=lambda t: datetime.strptime(t["deadline"], "%d/%m/%Y"))
-            for i, task in enumerate(ordered_tasks):
-                print (f"{i+1}. {task["title"]} -- {task["description"]} -- A completar antes del {task["deadline"]} --({task["status"]})")
+        print("No hay ninguna tarea guardada. selecciona otra opción")       
+    print_tasks(tasks)
+    print("Selecciona el numero de la tarea a completar. Si no, pulsa enter: ")
+    completed_task = input()
+    while completed_task != "":
+        try: 
+            tasks[int(completed_task)-1]["status"] = "Completada"
+            saving_tasks()
+            print("Tarea completada")
+            break
+        except (ValueError,IndexError): 
+            print("Selecciona una opción válida")
+            completed_task = input()
+    print("-------")
+    filter_tasks()
 
-        print("Selecciona una nueva acción")
+def filter_tasks():
+    completed_tasks = list(filter(lambda x: x["status"] == "Completada", tasks))
+    not_completed_tasks = list(filter(lambda x: x["status"] == "No Completada", tasks))
+    ordered_tasks = sorted(tasks, key=lambda t: datetime.strptime(t["deadline"], "%d/%m/%Y"))
+    print("Filtrar por Completadas:'c', Filtrar por No completadas: 'nc', Ordenar por fecha límite: 'o', Salir a menu: Enter")
+    
+    opciones = {
+        "c": completed_tasks,
+        "nc": not_completed_tasks,
+        "o": ordered_tasks,
+    }
+    
+    user_filter_choice = "001"
+    while user_filter_choice != "":
+        user_filter_choice = input("Elige una opción: ")
+        accion = opciones.get(user_filter_choice)
+        if accion: #si opciones(user_filter choice) existe:
+            print_tasks(accion) 
+        elif user_filter_choice == "":
+            print("volviendo al menú inicial")
+        else:
+            print("Opción no válida. Inténtalo de nuevo.")
+
+def print_tasks(n):
+    for i, task in enumerate(n):
+        print (f"{i+1}. {task["title"]} -- {task["description"]} -- A completar antes del {task["deadline"]} --({task["status"]})")
 
 def add_task():
     task_title = input("Escribe una nueva tarea: ")
@@ -70,14 +81,14 @@ def add_task():
     while True:
         date_input = input("Escribe la fecha limite para completar esta tarea, en formato dd-mm-aaaa (guiones incluidos): ")
         try: 
-            task_deadline = datetime.strptime(date_input, "%d-%m-%Y").strftime("%d/%m/%Y")
+            task_deadline = datetime.strptime(date_input, "%d-%m-%Y").strftime("%d/%m/%Y") #strptime toma la fecha del string si esta en el formato indicado. strftime la mostrara en el formato indicado.
             break
         except ValueError:
             print ("Ingrese una fecha válida en formato dd-mm-aaaa (guiones incluidos)")
 
     tasks.append({"title": task_title, "description": task_description, "deadline": task_deadline, "status": "No Completada"}) #añade bibliotecas a la lista tasks
     saving_tasks()
-    print ("Tarea añadida")
+    print ("Tarea añadida. Volviendo al menú inicial")
     print("-------")
     print ("Selecciona una nueva opción.")
 
@@ -101,7 +112,8 @@ def delete_task():
 
 if __name__ == "__main__":
     tasks = [{"title": "correr", "description": "correr 100m", "deadline": "02/11/2025", "status": "Completada"}, {"title": "fumar mucho", "description": "fumar mucho mucho", "deadline": "02/03/2026", "status": "No Completada"},{"title": "gritar", "description": "gritR 100m", "deadline": "02/11/2027", "status": "Completada"}]
-    see_task()
+    printed_menu()
+    choices()
     while len(tasks) != 0:
         tasks.pop(len(tasks)-1)
     saving_tasks()
